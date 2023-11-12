bf6e192a8940c1766458c37d3b8272bb3232013d
Danislav Konyarski <unstopable46@gmail.com>
2023-11-12 11:57:30 +0200

    new task

---
+++ JsFrontEnd/L3FunctionsAndStatementsLab/T5SimpleCalculator.js
@@ -0,0 +1,11 @@
+function simpleCalculator( num1, num2, operator) {
+  let operators = {
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2,
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+  };
+  return operators[operator](num1,num2);
+}
+
+console.log(simpleCalculator(5,5, "multiply"))
