0b66766ae0c0fc17160480d08912aefe1ccef145
Danislav Konyarski <unstopable46@gmail.com>
2023-11-12 15:50:55 +0200

    new task

---
+++ JsFrontEnd/L3FunctionsAndStatementsExercise/T7NxNMatrix.js
@@ -0,0 +1,13 @@
+function nxNMatrix(num) {
+    let returnStr = "";
+    for (let i = 0; i < num; i++) {
+        for (let j = 0; j < num; j++) {
+            returnStr+= num+" ";
+        }
+        returnStr= returnStr.trim()+"\n";
+    }
+
+    return returnStr;
+}
+
+console.log(nxNMatrix(3))
