6841318c449c549969c4b83542cd5abeadb5ae6e
Danislav Konyarski <unstopable46@gmail.com>
2023-11-28 18:18:05 +0200

    new task

--- JsFrontEnd/L5DOM IntroductionLab/02.Collect List Items/CollectListItems.js
+++ JsFrontEnd/L5DOM IntroductionLab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+  let textArr = document.querySelectorAll("ul li");
+  for (const iterator of textArr) {
+    document.getElementById("result").value += iterator.textContent + "\n";
+  }
 }
