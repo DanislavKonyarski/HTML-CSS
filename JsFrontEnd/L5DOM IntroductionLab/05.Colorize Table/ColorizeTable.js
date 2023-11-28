7a7cae32466714caeb0db78349a824a48b65febb
Danislav Konyarski <unstopable46@gmail.com>
2023-11-28 19:13:17 +0200

    new task

--- JsFrontEnd/L5DOM IntroductionLab/05.Colorize Table/ColorizeTable.js
+++ JsFrontEnd/L5DOM IntroductionLab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+  const elementsArr = document.querySelectorAll("table tr");
+  for (let index = 0; index < elementsArr.length; index++) {
+    if (index % 2 !== 0) {
+      elementsArr[index].style.backgroundColor = "Teal";
+    }
+  }
 }
