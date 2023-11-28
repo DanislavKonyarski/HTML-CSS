b7f568cc2dd7efc093be3c6dff78f9cbb2d6c1da
Danislav Konyarski <unstopable46@gmail.com>
2023-11-28 18:02:45 +0200

    new task

--- JsFrontEnd/L5DOM IntroductionLab/01.Edit Element/EditElement.js
+++ JsFrontEnd/L5DOM IntroductionLab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer) {
+  let text = ref.textContent;
+  let regex = new RegExp(match, "g");
+  ref.textContent = text.replace(regex, replacer);
 }
