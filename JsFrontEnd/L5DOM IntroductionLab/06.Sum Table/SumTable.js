0f293cd4ffaacfe468a568c90f1294df9a5218af
Danislav Konyarski <unstopable46@gmail.com>
2023-11-28 19:25:46 +0200

    new task

--- JsFrontEnd/L5DOM IntroductionLab/06.Sum Table/SumTable.js
+++ JsFrontEnd/L5DOM IntroductionLab/06.Sum Table/SumTable.js
@@ -1,3 +1,8 @@
 function sumTable() {
-
+  const priceArticles = document.querySelectorAll("td:nth-child(even)");
+  let sum = 0;
+  for (let index = 0; index < priceArticles.length; index++) {
+    sum += Number(priceArticles[index].textContent);
+  }
+  document.getElementById("sum").textContent = sum.toFixed(2);
 }
