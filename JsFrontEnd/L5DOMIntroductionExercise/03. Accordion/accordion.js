5ef60c7df24419dab9e7a282f9f57a277c5774e1
Danislav Konyarski <unstopable46@gmail.com>
2023-11-29 14:08:40 +0200

    new task

--- JsFrontEnd/L5DOMIntroductionExercise/03. Accordion/accordion.js
+++ JsFrontEnd/L5DOMIntroductionExercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.querySelector("span").textContent;
+    if (button === 'More') {
+        document.querySelector("span").textContent = "Less";
+        document.getElementById('extra').style.display = 'block';
+    }else{
+        document.querySelector("span").textContent = "More";
+        document.getElementById('extra').style.display = 'none';
+    }
+   
 }
