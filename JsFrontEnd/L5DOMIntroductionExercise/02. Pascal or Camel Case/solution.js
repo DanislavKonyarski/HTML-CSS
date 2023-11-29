2466d0d81eb18d3d7bc79c72bd8b197c87ab6be0
Danislav Konyarski <unstopable46@gmail.com>
2023-11-29 13:49:47 +0200

    new task

--- JsFrontEnd/L5DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
+++ JsFrontEnd/L5DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO...
+  const text = document.getElementById("text").value;
+  const command = document.getElementById("naming-convention").value;
+  let currentArr = text.toLowerCase().split(" ");
+  let result = "";
+
+  if (command === "Camel Case") {
+    for (let i = 0; i < currentArr.length; i++) {
+      if (i === 0) {
+        result += currentArr[i];
+      } else {
+        let firstChar = currentArr[i].charAt(0);
+        let firstCharToUpper = currentArr[i].charAt(0).toUpperCase();
+        result += currentArr[i].replace(firstChar, firstCharToUpper);
+      }
+    }
+  } else if (command === "Pascal Case") {
+    for (let i = 0; i < currentArr.length; i++) {
+      let firstChar = currentArr[i].charAt(0);
+      let firstCharToUpper = currentArr[i].charAt(0).toUpperCase();
+      result += currentArr[i].replace(firstChar, firstCharToUpper);
+    }
+  } else {
+    result = "Error!";
+  }
+  document.getElementById("result").textContent = result;
 }
