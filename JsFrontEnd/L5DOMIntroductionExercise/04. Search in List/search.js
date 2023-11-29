c052c86281226ff9a4d960f6a78cbe41acae5490
Danislav Konyarski <unstopable46@gmail.com>
2023-11-29 14:57:17 +0200

    new task

--- JsFrontEnd/L5DOMIntroductionExercise/04. Search in List/search.js
+++ JsFrontEnd/L5DOMIntroductionExercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+  const input = document.querySelectorAll("li");
+  const match = document.getElementById("searchText").value;
+  let count = 0;
+  for (let index = 0; index < input.length; index++) {
+   let currentStr = input[index].textContent;
+    if (currentStr.includes(match)) {
+      count++;
+      document.querySelector(
+        `ul :nth-child(` + (index + 1) + `)`
+      ).style.fontWeight = "bold";
+      document.querySelector(
+        `ul :nth-child(` + (index + 1) + `)`
+      ).style.textDecoration = "underline";
+    }
+  }
+  document.getElementById("result").textContent = `${count} matches found`;
 }
