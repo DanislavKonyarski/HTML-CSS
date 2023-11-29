cbac6caa95223266391849729145e57f69292c48
Danislav Konyarski <unstopable46@gmail.com>
2023-11-29 18:27:00 +0200

    new task

--- JsFrontEnd/L5DOMIntroductionExercise/05. Table - Search Engine/solution.js
+++ JsFrontEnd/L5DOMIntroductionExercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,17 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
-
-   }
+  function onClick() {
+    const items = document.querySelectorAll("tbody > tr ");
+    const matches = document.getElementById('searchField').value;
+    
+    for (let i = 0; i < items.length; i++) {
+     
+      if (items[i].textContent.includes(matches)) {
+         document.querySelectorAll("tbody > tr ")[i].classList.add('select');
+      }else{
+         document.querySelectorAll("tbody > tr ")[i].classList.remove('select');
+      }
+    }
+  }
 }
