d6578a35235ea3d48324fa751d7db85c95cab499
Danislav Konyarski <unstopable46@gmail.com>
2023-11-30 10:15:53 +0200

    new task

--- JsFrontEnd/L5DOMIntroductionExercise/06. Format the Text/solution.js
+++ JsFrontEnd/L5DOMIntroductionExercise/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value.split(".");
+  let count = 0;
+  let currentString = "";
+  for (let i = 0; i < input.length; i++) {
+    if((input.length-1) === i || count ===3){
+      let para = document.createElement('p');
+      para.textContent = currentString;
+      let divForOutput = document.getElementById('output');
+      divForOutput.appendChild(para);
+       count = 0
+      currentString ="";
+    }
+    count++;
+    currentString+=input[i]+".";
+    
+  }
 }
