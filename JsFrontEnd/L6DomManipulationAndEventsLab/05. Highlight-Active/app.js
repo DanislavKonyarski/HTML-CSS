61067f145a41e152cf1a276bf5e7f206c2d5cf11
Danislav Konyarski <unstopable46@gmail.com>
2023-12-04 14:31:31 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsLab/05. Highlight-Active/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsLab/05. Highlight-Active/app.js
@@ -1,3 +1,13 @@
 function focused() {
-    console.log('TODO:...');
+  const allElement = document.querySelectorAll("div div input");
+  for (let i = 0; i < allElement.length; i++) {
+    allElement[i].addEventListener("focus", focus);
+    allElement[i].addEventListener("blur", notFocus);
+    function focus() {
+      allElement[i].parentElement.classList.add("focused");
+    }
+    function notFocus() {
+        allElement[i].parentElement.classList.remove("focused");
+    }
+  }
 }
