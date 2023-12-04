92a5bf7a33ca4c538c7582760fefd8c6a26e7e47
Danislav Konyarski <unstopable46@gmail.com>
2023-12-04 13:57:40 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsLab/03. Add-Delete/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsLab/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+  const input = document.getElementById("newItemText").value;
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.textContent = "[Delete]";
+  a.href = "#";
+
+  if (input.length === 0) {
+    return;
+  }
+
+  li.textContent = input;
+  document.getElementById("items").appendChild(li).appendChild(a);
+  document.getElementById("newItemText").value = "";
+  a.addEventListener("click", deleteItem);
+
+  function deleteItem() {
+    li.remove();
+  }
 }
