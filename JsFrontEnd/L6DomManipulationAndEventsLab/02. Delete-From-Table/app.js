b4b9478b8034c9baf044655987e78b4961ce327f
Danislav Konyarski <unstopable46@gmail.com>
2023-12-04 07:35:53 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsLab/02. Delete-From-Table/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsLab/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const emails = Array.from(document.querySelectorAll("tbody tr :nth-child(2)"));
+  const input = document.querySelector("label input");
+  const result = document.getElementById("result");
+  
+
+  for (let i = 0; i < emails.length; i++) {
+    if (emails[i].textContent === input.value) {
+        emails[i].parentElement.parentElement.removeChild(emails[i].parentElement);
+        input.value = "";
+        result.textContent = "Deleted.";
+      } else {
+        result.textContent = "Not found.";
+      }  
+  }
 }
