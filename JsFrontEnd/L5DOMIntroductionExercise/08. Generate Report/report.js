80fea8c829e35e2353895cd9e3c272292795183e
Danislav Konyarski <unstopable46@gmail.com>
2023-12-04 06:51:45 +0200

    new task

--- JsFrontEnd/L5DOMIntroductionExercise/08. Generate Report/report.js
+++ JsFrontEnd/L5DOMIntroductionExercise/08. Generate Report/report.js
@@ -1,3 +1,31 @@
 function generateReport() {
-    //TODO
+    
+  const check = document.querySelectorAll("thead tr th input");
+  const information = document.querySelectorAll("tbody tr");
+  let informationForOutputArr = [];
+
+  let currentIndexArr = [];
+  for (let i = 0; i < check.length; i++) {
+    if (check[i].checked) {
+      currentIndexArr.push(i);
+    }
+  }
+
+  for (let i = 0; i < information.length; i++) {
+    const currentElements = Array.from(information[i].children);
+    let currentObject = {};
+    currentIndexArr.forEach((e) => {
+      const key = document
+        .querySelectorAll("thead tr th")
+        [e].textContent.trim()
+        .toLowerCase();
+      const value = currentElements[e].textContent;
+      currentObject[key] = value;
+    });
+    informationForOutputArr.push(currentObject);
+  }
+
+  let output = JSON.stringify(informationForOutputArr);
+  document.getElementById("output").value = output;
+
 }
