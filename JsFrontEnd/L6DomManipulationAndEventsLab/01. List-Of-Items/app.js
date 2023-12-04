b1767d654a9a4b7ca73ce004e9b0a0b4be74ea9c
Danislav Konyarski <unstopable46@gmail.com>
2023-12-04 07:04:12 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsLab/01. List-Of-Items/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsLab/01. List-Of-Items/app.js
@@ -1,3 +1,7 @@
 function addItem() {
-    console.log('TODO:...');
+    const input = document.getElementById("newItemText").value;
+    const li = document.createElement("li");
+    li.textContent = input;
+    document.getElementById("items").appendChild(li);
+    document.getElementById("newItemText").value = "";
 }
