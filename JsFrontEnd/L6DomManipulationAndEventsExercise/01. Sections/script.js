f1e77e0ca6d064f7ffabaeec18a52824cc06ad52
Danislav Konyarski <unstopable46@gmail.com>
2023-12-05 14:32:50 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsExercise/01. Sections/script.js
+++ JsFrontEnd/L6DomManipulationAndEventsExercise/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById("content");
+   for (let i = 0; i < words.length; i++) {
+      const text = words[i];
+      const div = document.createElement("div");
+      const p = document.createElement("p");
+      content.appendChild(div).appendChild(p).textContent = text;
+      p.style.display = "none";
+      div.addEventListener("click", clickOn);
+   }
+
+   function clickOn(e){
+      e.currentTarget.querySelector("p").style.display = "block";
+   }
 }
