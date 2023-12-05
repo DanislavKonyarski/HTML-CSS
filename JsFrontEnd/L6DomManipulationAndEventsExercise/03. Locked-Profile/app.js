00c24b6df7fe8e1d45e7c820f1531a6eeddd9f51
Danislav Konyarski <unstopable46@gmail.com>
2023-12-05 16:12:09 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsExercise/03. Locked-Profile/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsExercise/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const buttons = document.querySelectorAll("button");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", onClick);
+  }
+
+  function onClick(e) {
+    const isRadioButtonUnlockClick =
+      !e.currentTarget.parentElement.querySelector("input").checked;
+    const element =
+      e.currentTarget.parentElement.getElementsByTagName("div")[0];
+    const button = e.currentTarget;
+
+    if (isRadioButtonUnlockClick) {
+      if (button.textContent === "Show more") {
+        element.style.display = "block";
+        button.textContent = "Hide it";
+      } else {
+        element.style.display = "none";
+        button.textContent = "Show more";
+      }
+    }
+  }
 }
