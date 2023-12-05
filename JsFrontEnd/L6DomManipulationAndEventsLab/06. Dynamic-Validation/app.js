3c1b761cea6e2c1751180f18fee97e2026efc424
Danislav Konyarski <unstopable46@gmail.com>
2023-12-05 11:55:01 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsLab/06. Dynamic-Validation/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsLab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+  document.getElementById("email").addEventListener("change", function (e) {
+    let input = e.currentTarget;
+    const regex = new RegExp(/[A-Z,a-z, 0-9]+@\w+\.[a-z]+/gm);
+    console.log(e);
+    console.log(input);
+    if (!regex.test(input.value)) {
+      input.classList.add("error");
+    }else{
+        input.classList.remove("error");
+    }
+  });
 }
