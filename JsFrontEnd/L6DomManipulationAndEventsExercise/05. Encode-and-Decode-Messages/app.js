5fbae3c6ac1b6523a6e027abd6938d11f63b9f7e
Danislav Konyarski <unstopable46@gmail.com>
2023-12-06 17:34:45 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsExercise/05. Encode-and-Decode-Messages/app.js
+++ JsFrontEnd/L6DomManipulationAndEventsExercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,24 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  let [inputEncode, inputDecode] = document.querySelectorAll("textarea");
+  const [btnEncode, btnDecode] = document.querySelectorAll("button");
+
+  btnEncode.addEventListener("click", encode);
+  btnDecode.addEventListener("click", decode);
+
+  function encode() {
+    let encodeOutput = "";
+    inputEncode.value.split("").forEach((e) => {
+      encodeOutput += String.fromCharCode(e.charCodeAt(0) + 1);
+    });
+    inputDecode.value = encodeOutput;
+    inputEncode.value = "";
+  }
+
+  function decode() {
+    let encodeOutput = "";
+    inputDecode.value.split("").forEach((e) => {
+      encodeOutput += String.fromCharCode(e.charCodeAt(0) - 1);
+    });
+    inputDecode.value = encodeOutput;
+  }
 }
