9f4d7268f593d919ec5aa61bf63d4bc9975427d6
Danislav Konyarski <unstopable46@gmail.com>
2023-12-06 14:47:15 +0200

    new task

--- JsFrontEnd/L6DomManipulationAndEventsExercise/04. Fill-Dropdown/dropdown.js
+++ JsFrontEnd/L6DomManipulationAndEventsExercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+  let text = document.getElementById("newItemText");
+  let value = document.getElementById("newItemValue");
+  let select = document.getElementById("menu");
+
+  const options = document.createElement("option");
+
+  if (text.value !== "" && value.value !== "") {
+    options.value = value.value;
+    options.textContent = text.value;
+    select.appendChild(options);
+    text.value = "";
+    value.value = "";
+  }
 }
